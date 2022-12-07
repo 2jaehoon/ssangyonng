a54494cd4e6eadddfb137c5d109636b7a0727bc4
2jaehoon <dlwogns4521@naver.com>
2022-12-07 13:18:57 +0900

    이클립스 1207

---
+++ src/day1207/HelloEclipse.java
@@ -0,0 +1,35 @@
+package day1207;
+
+//닷 주석은 알트 슈프트 j
+/**
+ * 이클립스의 사용법.
+ * 
+ * @author 내가
+ *
+ */
+public class HelloEclipse {
+	// 닷 주석은 알트 슈프트 j
+	/**
+	 * main method => java application을 만들 때 정의하는 method
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		// alt 윗방향키ㅡ 아래방향키로 자유자재로 소스코드 움직일 수 있다.
+		// ctrl d 한줄 삭제
+		// ctrl 스페이스 자동으로 찾아줌
+		// 커서가 있는 한줄을 복사하고 싶으면 ctrl alt 아래방향키, 윗방향키
+		// ctrl 슬래시를 쓰면 그 줄을 주석처리할 수 있다. 걸고 빼기 가능
+		// 여러줄 주석은 ctrl shift 슬래시인데 걸기만하고 뺄 순 없다.
+		// 닷 주석은 알트 슈프트 j
+		// ctrl shift f 정렬해주는 것  >>jsp나 htmp은 이거 쓰면 큰일남
+		int i = 0;
+		System.out.println(i);
+
+		System.out.println("안녕 이클립스!");
+		
+	}
+
+}
